2fcfbb7b5e77af861869d9ca98d65881765087f1
ashil rahim <ashilrahim2@gmail.com>
2025-10-27 18:59:56 +0530

    added some style chngs

--- app/(auth)/login/loading.tsx
+++ app/(auth)/login/loading.tsx
@@ -1,7 +1,17 @@
+import { Spinner } from "@/components/ui/spinner";
+
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <p className="text-gray-500 animate-pulse">Redirecting...</p>
+    <div className="flex items-center justify-center h-screen bg-background">
+      <div className="text-center space-y-3">
+        <p className="text-xl md:text-2xl font-sans font-medium text-foreground animate-pulse">
+          Redirecting to your dashboard...
+        </p>
+        <div className="flex justify-center">
+          <Spinner />
+        </div>
+      </div>
     </div>
   );
 }
+
