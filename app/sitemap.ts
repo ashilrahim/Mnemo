e228eaf2299d741384e11247f267e84c8d845cee
ashil rahim <ashilrahim2@gmail.com>
2025-11-18 09:57:32 +0530

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://mnemo-pi.vercel.app/",
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: "https://mnemo-pi.vercel.app/pricing",
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: "https://mnemo-pi.vercel.app/about",
+      lastModified: new Date().toISOString(),
+    },
+  ];
+}
